d251fb7f3045165e5838bc531969d7c5dcc64df6
ktochow1 <ktochow1@binghamton.edu>
2018-08-10 02:19:03 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -7,7 +7,7 @@ def fizzbuzz(int)
     return "FizzBuzz"
   elsif int % 5 == 0
     return "Buzz"
-  elsif int % 3 == 0 && 5 == 0
+  elsif int % 15 == 0
     return "Fizz"
   else int % !15 == 0
     return nil
