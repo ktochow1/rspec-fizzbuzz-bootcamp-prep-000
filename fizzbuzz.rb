ab0378bd9cf2d3a909e66bf8a29e1b4dd59f584a
ktochow1 <ktochow1@binghamton.edu>
2018-08-10 02:20:44 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,7 +4,7 @@
 
 def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
-    return "FizzBuzz"
+    return "Fizz"
   elsif int % 5 == 0
     return "Buzz"
   elsif int % 15 == 0
