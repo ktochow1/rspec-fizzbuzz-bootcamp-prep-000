8244ec6f32b02570afab3d38d874fb918fc8c939
ktochow1 <ktochow1@binghamton.edu>
2018-08-10 01:40:15 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,6 +2,8 @@
 # See README.md for instructions on how to do this
 
 
-def fizzbuzz
-  fizz_3 = fizzbuzz(3)
+def fizzbuzz(int)
+  if int % 3 == 0 # if the number int is divisible by 3
+    "Fizz" # Go fizz
+  end
 end
