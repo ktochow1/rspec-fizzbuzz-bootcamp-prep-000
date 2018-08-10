b0873e95d8fce4be1551a58bf693bfba454ae190
ktochow1 <ktochow1@binghamton.edu>
2018-08-10 01:32:58 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz
-  fizz_3 = fizzbuzz
-  fizz_3 == 3
+  if fizzbuzz == 3
+    puts "Fizz"
 end
